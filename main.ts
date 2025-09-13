187985adfc6703d8acb60eacf72011955b803848
kawagh <kawagh.dev@gmail.com>
2025-09-13 14:14:04 +0900

    [feat] show version with --version, -v

--- main.ts
+++ main.ts
@@ -2,8 +2,19 @@ import { parseArgs } from "@std/cli/parse-args";
 import { listVueComponentDependencies } from "./src/analyze.ts";
 import { makeDOTGraphString } from "./src/draw.ts";
 
+const VERSION = "0.1.0";
+
 const main = async () => {
-  const args = parseArgs(Deno.args);
+  const args = parseArgs(Deno.args, {
+    boolean: ["version"],
+    alias: {
+      v: "version",
+    }
+  });
+  if(args.version) {
+    console.log(VERSION);
+    Deno.exit(0);
+  }
   const directory = args._.length == 0 ? "." : String(args._[0]);
   const edges = await listVueComponentDependencies(directory);
   const str = makeDOTGraphString(edges, {});
