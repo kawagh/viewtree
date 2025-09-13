7b66c1d384aadeb0f4d38e32edc998f52ed87dd3
kawagh <kawagh.dev@gmail.com>
2025-09-13 11:51:56 +0900

    parse args and print its sum

--- main.ts
+++ main.ts
@@ -1,8 +1,22 @@
+import { parseArgs } from "@std/cli/parse-args";
+
 export function add(a: number, b: number): number {
   return a + b;
 }
 
+const main = () => {
+  const args = parseArgs(Deno.args);
+  if (args._.length != 2) {
+    console.error("引数は2つ指定してください");
+    Deno.exit(1);
+  }
+  const n1 = Number(args._[0]);
+  const n2 = Number(args._[1]);
+  console.log(args);
+  console.log(`Add ${n1} + ${n2} = ${add(n1, n2)}`);
+};
+
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+  main();
 }
