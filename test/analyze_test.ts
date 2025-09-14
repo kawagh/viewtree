7d323cc8708651e411bd2f33a4505bd497653554
kawagh <kawagh.dev@gmail.com>
2025-09-14 12:03:26 +0900

    [define] deno task test to allow read permission

--- test/analyze_test.ts
+++ test/analyze_test.ts
@@ -8,21 +8,23 @@ import { ImportInfo } from "../src/types.ts";
 Deno.test("extractComponentName", () => {
   assertEquals(extractComponentName("@/components/Header.vue"), "Header");
 });
-
-Deno.test("listVueComponentDependencies", async (t) => {
-  await t.step({
-    name: "コンポーネントの依存関係が全て検出されていること",
-    async fn() {
-      const result = await listVueComponentDependencies("./test/resource");
-      const expected: ImportInfo[] = [
-        { from: "ListView", to: "Header" },
-        { from: "ListView", to: "Footer" },
-        { from: "ListView", to: "Table" },
-        { from: "Table", to: "TableRow" },
-        { from: "Table", to: "TableHeader" },
-      ];
-      assertArrayIncludes(result, expected);
-      assertEquals(result.length, expected.length);
-    },
-  });
+Deno.test({
+  name: "listVueComponentDependencies",
+  fn: async (t) => {
+    await t.step({
+      name: "コンポーネントの依存関係が全て検出されていること",
+      async fn() {
+        const result = await listVueComponentDependencies("./test/resource");
+        const expected: ImportInfo[] = [
+          { from: "ListView", to: "Header" },
+          { from: "ListView", to: "Footer" },
+          { from: "ListView", to: "Table" },
+          { from: "Table", to: "TableRow" },
+          { from: "Table", to: "TableHeader" },
+        ];
+        assertArrayIncludes(result, expected);
+        assertEquals(result.length, expected.length);
+      },
+    });
+  },
 });
