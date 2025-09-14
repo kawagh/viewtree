f7ec594e5995aca2bcc583105fb79e9ffee654c2
kawagh <kawagh.dev@gmail.com>
2025-09-14 11:59:04 +0900

    [test] listVueComponentDependencies

--- test/analyze_test.ts
+++ test/analyze_test.ts
@@ -1,6 +1,28 @@
-import { assertEquals } from "@std/assert/equals";
-import { extractComponentName } from "../src/analyze.ts";
+import { assertArrayIncludes, assertEquals } from "@std/assert";
+import {
+  extractComponentName,
+  listVueComponentDependencies,
+} from "../src/analyze.ts";
+import { ImportInfo } from "../src/types.ts";
 
 Deno.test("extractComponentName", () => {
   assertEquals(extractComponentName("@/components/Header.vue"), "Header");
+});
+
+Deno.test("listVueComponentDependencies", async (t) => {
+  await t.step({
+    name: "コンポーネントの依存関係が全て検出されていること",
+    async fn() {
+      const result = await listVueComponentDependencies("./test/resource");
+      const expected: ImportInfo[] = [
+        { from: "ListView", to: "Header" },
+        { from: "ListView", to: "Footer" },
+        { from: "ListView", to: "Table" },
+        { from: "Table", to: "TableRow" },
+        { from: "Table", to: "TableHeader" },
+      ];
+      assertArrayIncludes(result, expected);
+      assertEquals(result.length, expected.length);
+    },
+  });
 });
