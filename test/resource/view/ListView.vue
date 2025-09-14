d0f15b271d2e0c3c085d90e940135c352057c4b7
kawagh <kawagh.dev@gmail.com>
2025-09-14 11:44:06 +0900

    [add] テスト用の.vueファイルを追加

---
+++ test/resource/view/ListView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import Header from '../component/Header.vue';
+import Footer from '../component/Footer.vue';
+import Table from '../component/Table.vue';
+</script>
+<template>
+</template>
